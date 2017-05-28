2a84693393a7c91f430559229c86a9a49c990358
igoldyrev <goldirev12@gmail.com>
2017-05-28 13:42:47 +0500

    Добавлены рамки вокруг ссылок как в основном каталоге

--- templates/model.php
+++ templates/model.php
@@ -1,4 +1,6 @@
-<h1 align="center">Фаркопы для <?php echo $auto['model']; ?> </h1>
+<h1 align="center">Фаркопы для <?php echo $auto['marka'][1]; ?> </h1>
 <?php foreach ($audi as $au): ?>
+	<div class="index">
 		<a href="/catalog.php?farkop=<?php echo $au['marka']; ?>&model=<?php echo $au['model']; ?>"><?php echo $au['model']; ?></a>
+	</div>	
 <?php endforeach; ?>
