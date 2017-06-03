fca6a6c8b5733324155cecf95fd128fae558021a
igoldyrev <goldirev12@gmail.com>
2017-06-03 15:41:52 +0500

    Файл удален

--- templates/main.php
+++
@@ -1,7 +0,0 @@
-<h1 align="center">Каталог фаркопов</h1>
-<?php foreach ($auto as $marka): ?>
-<div class="index">
-	<div align="center"><a class="index" href="/catalog.php?farkop=<?php echo $marka['marka']; ?>"><img class="index" src="<?php echo $marka['img']; ?>"></a></div>
-	<div  align="center"><a href="/catalog.php?farkop=<?php echo $marka['marka']; ?>"><?php echo $marka['marka']; ?></a></div>
-</div>
-<?php endforeach; ?>
