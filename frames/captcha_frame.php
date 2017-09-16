28eb38a8d5053a1ad8df1606390f91161b7230ce
igoldyrev <goldirev12@gmail.com>
2017-09-16 14:47:45 +0500

    Поле для ввода капчи

---
+++ frames/captcha_frame.php
@@ -0,0 +1,3 @@
+<span class="label_top">Введите числа с картинки:</span><br>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/captcha/captcha.php");?>   
+<input name="captcha" type="text" size="6" maxlength="5">
