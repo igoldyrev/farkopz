869436786d153e6b676d1d07bda33d8efa195daf
igoldyrev <goldirev12@gmail.com>
2017-09-16 12:57:55 +0500

    Добавлены паттерны для избежания ввода в форму спама

--- zayavka.php
+++ zayavka.php
@@ -1,19 +1,19 @@
 <div id="zakaz">
 <h2>Заказать фаркоп</h2>
-<form action="/zakaz.php" method="post">
+<form action="/zakaz" method="post">
 <span class="label_top">Ваше имя:</span>
 <div class="better-placeholder">
-  <input type="text" name="name" required="required" class="better-placeholder__input" placeholder="Введите Ваше имя">
+  <input type="text" name="name" required="required" pattern="[А-Яа-яЁё]{2,}" class="better-placeholder__input" placeholder="Введите Ваше имя">
   <label for="name" class="better-placeholder__label">Введите Ваше имя</label>
 </div><br>
 <span class="label_top">Ваш телефон:</span>
 <div class="better-placeholder">
-  <input type="text" name="phone" class="better-placeholder__input" required="required" placeholder="Введите номер телефона">
+  <input type="text" name="phone" class="better-placeholder__input" required="required" pattern="[0-9]{10,11}" placeholder="Введите номер телефона">
   <label for="phone" class="better-placeholder__label">Введите номер телефона</label>
 </div><br>
 <span class="label_top">Марка машины:</span>
 <div class="better-placeholder">
-  <input type="text" name="auto" class="better-placeholder__input" placeholder="Введите марку автомобиля">
+  <input type="text" name="auto" class="better-placeholder__input" pattern="^[A-Za-zА-Яа-яЁё0-9\s]+$" placeholder="Введите марку автомобиля">
   <label for="auto" class="better-placeholder__label">Введите марку автомобиля</label>
 </div><br>
 <span class="label_top">Тип кузова:</span>
@@ -32,7 +32,7 @@
 </div><br>
 <span class="label_top">Текст заявки</span>
 <div class="better-placeholder">
-<textarea name="text" class="better-placeholder__input" placeholder="Введите текст заявки"></textarea>
+<textarea name="text" class="better-placeholder__input" pattern="^[A-Za-zА-Яа-яЁё0-9\s]+$" placeholder="Введите текст заявки"></textarea>
   <label for="text" class="better-placeholder__label">Введите текст заявки</label>
 </div><br>
 <div align="center">
