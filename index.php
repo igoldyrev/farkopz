d555e9f426268c096ac25a5a1f4813f8ba198ccd
igoldyrev <goldirev12@gmail.com>
2018-03-06 23:52:03 +0500

    develop / Правка главной страницы

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/headtags.php"); 
-include ($_SERVER["DOCUMENT_ROOT"]."/frames/keywords.php"); 
-	echo "<title> $titleconst"; echo $keywords[0][title]; echo "</title>";
-	echo "<meta name='description' content='"; echo $keywords[0][description]; echo "'/>";
-	echo "<meta name='keywords' content='"; echo $keywords[0][keywords]; echo "'/>";
-	
-include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/header.html");?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatags.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/header.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/main.html");
 
-<div id="content">
-<?php
-include ($_SERVER["DOCUMENT_ROOT"]."/frames/auto_array.php");
 
-require('brands.php');
-require('montazh.php');
-require('zayavka.php'); 
-require('contacts.php'); ?>
-</div>
-<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");
-include ($_SERVER["DOCUMENT_ROOT"]."/frames/counters.html");?>
+include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/counters.html");
