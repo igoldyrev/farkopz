4a5c787a64dcc57c3cac2e8e2a62e18ff29bb549
igoldyrev <goldirev12@gmail.com>
2018-03-06 23:49:59 +0500

    develop / Блоки перемещены

---
+++ backend/blocks/metatags.php
@@ -0,0 +1,25 @@
+<?php session_start();
+$titleconst = "Автобагаж - купить фаркоп в Перми";
+echo "<title>$titleconst</title>"
+?>
+<html lang="ru">
+
+<!-- meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index,follow">
+
+<!-- meta property -->
+<meta property="place:location:latitude" content="58.03593904782619">
+<meta property="place:location:longitude" content="56.2000031453342">
+<meta property="business:contact_data:street_address" content="Ул. Спешилова 102/29">
+<meta property="business:contact_data:locality" content="Пермь">
+<meta property="business:contact_data:email" content="autobagaz@yandex.ru">
+<meta property="business:contact_data:website" content="https://autobagaz.ru">
+<meta property="og:url" content="https://autobagaz.ru">
+<meta property="og:site_name" content="Автобагаж">
+
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+<link rel="stylesheet" href="../build/farkopz.css">
