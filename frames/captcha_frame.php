188613e72d99578331b53d76f38f239e7278a92e
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-07 17:04:12 +0500

    develop / Удалены старые блоки

--- frames/captcha_frame.php
+++
@@ -1,3 +0,0 @@
-<span class="label_top">Введите числа с картинки:</span><br>
-<?php include ($_SERVER["DOCUMENT_ROOT"]."/captcha/captcha.php");?>   
-<input name="captcha" type="text" size="6" maxlength="5">
