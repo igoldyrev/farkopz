0f04392caf7143697f3abb0bd2090932cd87aefa
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-07 18:20:54 +0500

    develop / Верстка блока с этапами работы

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatags.php");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/header.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/main.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/steps.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/montazh.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/form.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/contacts.html");
