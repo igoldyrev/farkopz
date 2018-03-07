259579f25940a635868d218060ebea2b702ca8de
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-07 17:32:13 +0500

    develop / Верстка блока с контактами

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/header.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/main.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/montazh.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/form.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/contacts.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/src/html.blocks/footer.html");
-
-
-include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/counters.html");
