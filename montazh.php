bb0315a9e2ec5038cce391b25ddc672bd4e1560f
igoldyrev <goldirev12@gmail.com>
2018-03-10 15:50:51 +0500

    develop / Более не нужно

--- montazh.php
+++
@@ -1,9 +0,0 @@
-<h2>Монтаж фаркопов в Перми</h2>
-<div id="montazh">
-<p>Уважаемые клиенты! Заводская гарантия на автомобиль сохраняется при установке фаркопа и любого другого дополнительного оборудования, если установка выполняется в сертифицированном техцентре. Монтаж фаркопов, купленных в нашем магазине, производится в сертифицированном сервисном центре, по адресу Спешилова 80.</p>
-<table cellspacing="0" align="center">
-<tr><td class="producttablecaption" align="center">Оборудование</td><td class="producttablecaption" align="center">Цена</td></tr>
-<tr><td class="producttable" align="center">Монтаж фаркопа без подключения электрики</td><td class="producttable" align="center">2500 рублей</td></tr>
-<tr><td class="producttable" align="center">Монтаж фаркопа с универсальной электрикой</td><td class="producttable" align="center">3500 рублей</td></tr>
-<tr><td class="producttable" align="center">Монтаж фаркопа с электрикой с блоком согласования SMART CONNECT</td><td class="producttable" align="center">5000 рублей</td></tr></table>
-<p>Установка фаркопов в установочном центре без предварительной записи не производится. Время уточняйте по телефону!</p></div>
