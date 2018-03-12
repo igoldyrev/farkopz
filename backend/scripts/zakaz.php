8b3ed730795b9c9323be7573fe02d4055f50717d
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-12 16:32:18 +0500

    develop / Уменььшена форма для более удобного пользования на мобильных устройствах

--- backend/scripts/zakaz.php
+++ backend/scripts/zakaz.php
@@ -17,36 +17,26 @@ include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php"); ?>
                     $name = $_POST['name'];
                     $phone = $_POST['phone'];
                     $auto = $_POST['auto'];
-                    $kuzov = $_POST['kuzov'];
-                    $year = $_POST['year'];
                     $text = $_POST['text'];
                     //Первая функция преобразует все символы, которые пользователь попытается добавить в форму
                     $name = htmlspecialchars($name);
                     $phone = htmlspecialchars($phone);
                     $auto = htmlspecialchars($auto);
-                    $kuzov = htmlspecialchars($kuzov);
-                    $year = htmlspecialchars($year);
                     $text = htmlspecialchars($text);
                     //Вторая функция декодирует url, если пользователь попытается его добавить в форму
                     $name = urldecode($name);
                     $phone = urldecode($phone);
                     $auto = urldecode($auto);
-                    $kuzov = urldecode($kuzov);
-                    $year = urldecode($year);
                     $text = urldecode($text);
                     //Третьей функцией мы удалим пробелы с начала и конца строки, если таковые имеются
                     $name = trim($name);
                     $phone = trim($phone);
                     $auto = trim($auto);
-                    $kuzov = trim($kuzov);
-                    $year = trim($year);
                     $text = trim($text);
                     //Заносим данные из формы в переменные
                     $name = $_REQUEST['name'];
                     $phone = $_REQUEST['phone'];
                     $auto = $_REQUEST['auto'];
-                    $kuzov = $_REQUEST['kuzov'];
-                    $year = $_REQUEST['year'];
                     $text = $_REQUEST['text'];
 
                     include($_SERVER["DOCUMENT_ROOT"] . "/backend/scripts/mails.php");
